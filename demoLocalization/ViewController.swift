42b001846dfac9f4a28a5cccc1712663cfd4d753
SanketMaradiya01 <sanketmaradiya@nimapinfotech.com>
2024-02-17 10:38:44 +0530

    Localisation

--- demoLocalization/ViewController.swift
+++ demoLocalization/ViewController.swift
@@ -1,19 +1,47 @@
-//
-//  ViewController.swift
-//  demoLocalization
-//
-//  Created by Level Supermind on 16/02/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var txtInfo: UITextView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var langPicker: UIPickerView!
+    
+        var languageList: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        languageList = ["English", "French", "German","Hindi"]
+        //by default
+        changeLanguage(lang: "en")
     }
-
-
+    func numberOfComponents( in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int)  -> Int {
+        return languageList.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return languageList[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if languageList[row] == "French" {
+            changeLanguage(lang: "fr")
+        } else if languageList[row] == "German" {
+            changeLanguage(lang: "de")
+        } else if languageList[row] == "English" {
+            changeLanguage(lang: "en")
+        } else {
+            changeLanguage(lang: "hi")
+        }
+    }
+    func changeLanguage(lang: String) {
+        lblTitle.text = "title".localizeString(string: lang)
+        txtInfo.text = "info".localizeString(string: lang)
+    }
 }
-
+extension String {
+    func localizeString(string: String) -> String {
+        let path = Bundle.main.path(forResource: string, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
+}
---
+++ demoLocalization/customTVC.swift
@@ -0,0 +1,26 @@
+//
+//  customTVC.swift
+//  demoLocalization
+//
+//  Created by Level Supermind on 16/02/24.
+//
+
+import UIKit
+
+class customTVC: UITableViewCell {
+
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
